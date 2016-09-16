8a68c2c563b5cd9bd607d1d3ac14594d80bd9fcb
Adrian <drianhacon173@example.com>
2016-09-15 23:35:25 -0600

    update sh file

--- make.sh
+++ make.sh
@@ -2,11 +2,11 @@
 
 rm Proyecto0
 cd src 
-#rm rutas pending mochila
-rm equipos
+rm rutas pending mochila equipos
+
 #gcc -rdynamic -o rutas rutas.c -Wall `pkg-config --cflags --libs gtk+-3.0` -export-dynamic -Wunused-function
-#gcc -rdynamic -o pending pending.c -Wall `pkg-config --cflags --libs gtk+-3.0` -export-dynamic 
-#gcc -rdynamic -o mochila mochila.c -Wall `pkg-config --cflags --libs gtk+-3.0` -export-dynamic
+gcc -rdynamic -o pending pending.c -Wall `pkg-config --cflags --libs gtk+-3.0` -export-dynamic 
+gcc -rdynamic -o mochila mochila.c -Wall `pkg-config --cflags --libs gtk+-3.0` -export-dynamic
 gcc -rdynamic -o equipos equipos.c -Wall `pkg-config --cflags --libs gtk+-3.0` -export-dynamic 
 cd ..
 
